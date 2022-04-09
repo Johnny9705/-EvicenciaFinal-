c1428671320f0f3dd6de7286725221cbe13a1fa0
Mike <Mike@Mike-PC>
2022-04-08 22:16:37 -0600

    Se agrego repartidor

---
+++ src/CodigoFuente/Envio.java
@@ -0,0 +1,21 @@
+package CodigoFuente;
+
+public class Envio {
+	int IDenvio;
+	int IDCliente;
+	
+	public int getIDenvio() {
+		return IDenvio;
+	}
+	public void setIDenvio(int iDenvio) {
+		IDenvio= iDenvio;
+	}
+	public int getIDCliente() {
+		return IDCliente;
+	}
+	public void setIDCliente(int iDCliente) {
+		IDCliente = iDCliente;
+	}
+	
+	
+}
